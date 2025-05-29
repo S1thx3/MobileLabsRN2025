7c1ea324366f93dc5e3d3522638a45cd7913fd1e
ALex Lebiga <skebs7101@gmail.com>
2025-05-29 23:43:59 +0300

    create lab2 PJ SteamAppClone using Expo Router

---
+++ lab2/SteamAppClone/theme/lightTheme.ts
@@ -0,0 +1,12 @@
+import { AppTheme } from './darkTheme'; 
+export const lightTheme: AppTheme = { 
+  name: 'light' as const,
+  background: '#F0F0F0',
+  text: '#121212',
+  primary: '#1DB954',
+  cardBackground: '#FFFFFF',
+  tabBarBackground: '#FFFFFF',
+  tabBarActive: '#1DB954',
+  tabBarInactive: '#757575',
+
+};
